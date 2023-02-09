abbdf02b528397974ef16889ab111656eaa709e5
josephgrabinger <joseph.grabinger@yahoo.de>
2023-02-09 21:09:47 +0100

    moved TextStyle to its own file

--- lib/widgets/text.dart
+++ lib/widgets/text.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/widgets.dart' show Text;
 
-import 'package:pdf/widgets.dart' as pw show Text, TextStyle;
+import 'package:pdf/widgets.dart' as pw show Text;
 
-import 'package:teaplates/args/color.dart';
-import 'package:teaplates/args/font_style.dart';
-import 'package:teaplates/args/font_weight.dart';
+import '/args/text_style.dart';
 
 
 extension TextConverter on Text {
   pw.Text toPDFWidget() => pw.Text(data!,
-    style: style != null ? pw.TextStyle(
-      color: style!.color != null ? style!.color!.toPdfColor() : null,
-      fontSize: style!.fontSize,
-      fontStyle: style!.fontStyle != null ? style!.fontStyle!.toPdfFontStyle() : null,
-      fontWeight: style!.fontWeight != null ? style!.fontWeight!.toPdfFontWeight() : null,
-      height: style!.height,
-      letterSpacing: style!.letterSpacing,
-      wordSpacing: style!.wordSpacing,
-      // TODO add remaining style options
-    ) : null,
+    maxLines: maxLines,
+    softWrap: softWrap,
+    textScaleFactor: textScaleFactor ?? 1.0,
+    style: style?.toPdfTextStyle(),
   );
 }
