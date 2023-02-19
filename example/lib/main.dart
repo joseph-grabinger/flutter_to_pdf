192d94e4074559fae6b6255f81af426ae85c012a
josephgrabinger <joseph.grabinger@yahoo.de>
2023-02-19 20:12:17 +0100

    fix export of initial widget

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -58,76 +58,74 @@ class _MyHomePageState extends State<MyHomePage> {
           Builder(
             builder: (BuildContext ctx) {
               exportContext = ctx;
-              return Container(
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children:  [
-                            Text('Dunef UG (haftungsbeschränkt)'),
-                            Text('Mock-Document',
-                              style: TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold,
-                              ),
+              return Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children:  [
+                          Text('Dunef UG (haftungsbeschränkt)'),
+                          Text('Mock-Document',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
                             ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            const Text('Date: ',
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                            Text(DateFormat('MM.dd.yyyy').format(DateTime.now())),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 50,
-                          child: Image.asset('assets/logo.png'),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: Container(
-                        height: 100,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
-                          border: Border.all(color: Colors.grey)
-                        ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Text('Date: ',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                          Text(DateFormat('MM.dd.yyyy').format(DateTime.now())),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 50,
+                        child: Image.asset('assets/logo.png'),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Container(
+                      height: 100,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(color: Colors.grey)
                       ),
                     ),
-                    Row(
-                      children: [
-                        Container(
-                          height: 100,
-                          width: 100,
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                              image: NetworkImage('http://i.pravatar.cc/300'),
-                            )
-                          ),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: NetworkImage('http://i.pravatar.cc/300'),
+                          )
                         ),
-                        const SizedBox(width: 5.0),
-                        SizedBox(
-                          width: 200,
-                          height: 50,
-                          child: TextField(
-                            controller: TextEditingController(),
-                            decoration: const InputDecoration(
-                            label: Text('Name'), border: OutlineInputBorder(),
-                          )),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                      const SizedBox(width: 5.0),
+                      SizedBox(
+                        width: 200,
+                        height: 50,
+                        child: TextField(
+                          controller: TextEditingController(),
+                          decoration: const InputDecoration(
+                          label: Text('Name'), border: OutlineInputBorder(),
+                        )),
+                      ),
+                    ],
+                  ),
+                ],
               );
             },
           ),
--- lib/traversal.dart
+++ lib/traversal.dart
@@ -22,7 +22,8 @@ import 'widgets/list_view.dart';
 import 'widgets/grid_view.dart';
 
 
-/// Exports the provided [context] to a PDF file.
+/// Exports the provided [context] to a PDF file
+/// and applies the [textFieldOptions].
 Future<pw.Document> exportToPDF(BuildContext context, {
   TextFieldOptions textFieldOptions = const TextFieldOptions.none(),
 }) async {
@@ -44,110 +45,121 @@ Future<pw.Document> exportToPDF(BuildContext context, {
 Future<pw.Widget?> traverseWidgetTree(BuildContext context, TextFieldOptions options) async {
   pw.Widget? pdfWidget;
   Element? element;
+
   context.visitChildElements((Element e) async {
     print('Initial: $e');
     element = e;
   });
 
-  pdfWidget = (await visit(element!, options)).first;
+  pdfWidget = (await matchWidget(element!, [], options)).first;
+
+  print('initial PDF Widget: $pdfWidget');
 
   return pdfWidget;
 }
 
-  /// Recursive helper to visit all child elements of the provided [element].
-  Future<List<pw.Widget>> visit(Element element, TextFieldOptions options) async {
-    List<pw.Widget> children = [];
-    List<Element> elements = [];
+/// Recursive helper to visit all child elements of the provided [element].
+Future<List<pw.Widget>> visit(Element element, TextFieldOptions options) async {
+  List<Element> elements = [];
 
-    element.visitChildElements((Element element) async {
-      print('Element ${element.depth}: ${element.widget}');
-      elements.add(element);
-    });
+  element.visitChildElements((Element element) async {
+    print('Element ${element.depth}: ${element.widget}');
+    elements.add(element);
+  });
 
-    for (Element e in elements) {
-      final Widget widget = e.widget;
+  List<pw.Widget> children = [];
 
-      switch (widget.runtimeType) {
-        case MergeSemantics: //anchor: end of widget tree
-          print('Reached Anchor');
-          return [];
-        case Container:
-          final List childWidgets = await visit(e, options);
-          children.add(await (widget as Container).toPdfWidget(
-            childWidgets.isNotEmpty ? childWidgets.first : null
-          ));
-          break;
-        case Center:
-          final List childWidgets = await visit(e, options);
-          children.add((widget as Center).toPdfWidget(
-            childWidgets.isNotEmpty ? childWidgets.first : null
-          ));
-          break;
-        case SizedBox:
-          final List childWidgets = await visit(e, options);
-          children.add((widget as SizedBox).toPdfWidget(
-            childWidgets.isNotEmpty ? childWidgets.first : null
-          ));
-          break;
-        case Padding:
-          final List childWidgets = await visit(e, options);
-          children.add((widget as Padding).toPdfWidget(
-            childWidgets.isNotEmpty ? childWidgets.first : null
-          ));
-          break;
-        case Align:
-          final List childWidgets = await visit(e, options);
-          children.add((widget as Align).toPdfWidget(
-            childWidgets.isNotEmpty ? childWidgets.first : null
-          ));
-          break;
-        case Positioned:
-          children.add((widget as Positioned).toPdfWidget((await visit(e, options)).first));
-          break;
-        case Expanded:
-          children.add((widget as Expanded).toPdfWidget((await visit(e, options)).first));
-          break;
-        case Flexible:
-          children.add((widget as Flexible).toPdfWidget((await visit(e, options)).first));
-          break;
-        case Text:
-          children.add((widget as Text).toPdfWidget());
-          break;
-        case TextField:
-          children.add((widget as TextField).toPdfWidget(options));
-          break;
-        case Divider:
-          children.add((widget as Divider).toPdfWidget());
-          break;
-        case Image:
-          children.add(await (widget as Image).toPdfWidget());
-          break;
-        case Column:
-          children.add((widget as Column).toPdfWidget(await visit(e, options)));
-          break;
-        case Row:
-          children.add((widget as Row).toPdfWidget(await visit(e, options)));
-          break;
-        case Stack:
-          children.add((widget as Stack).toPdfWidget(await visit(e, options)));
-          break;
-        case ListView:
-          children.add((widget as ListView).toPdfWidget(await visit(e, options)));
-          break;
-        case GridView:
-          children.add((widget as GridView).toPdfWidget(await visit(e, options)));
-          break;
-        default:
-          print('Uncaught: ${widget.runtimeType}');
-          final List childWidgets = await visit(e, options);
-          if (childWidgets.isNotEmpty) {
-            children.addAll(childWidgets.map((x) => x));
-          }
-          break;
-      }
-    }
-
-    print('returned: $children');
-
-    return children;
+  for (Element e in elements) {
+    children = await matchWidget(e, children, options);
   }
+
+  print('returned: $children');
+
+  return children;
+}
+
+/// Matches the widget provided as [element]
+/// and appends the corresponding [pw.Widget] to [children].
+Future<List<pw.Widget>> matchWidget(Element element, List<pw.Widget> children, TextFieldOptions options) async {
+  final Widget widget = element.widget;
+
+  switch (widget.runtimeType) {
+    case MergeSemantics: //anchor: end of widget tree
+      print('Reached Anchor');
+      return [];
+    case Container:
+      final List childWidgets = await visit(element, options);
+      children.add(await (widget as Container).toPdfWidget(
+        childWidgets.isNotEmpty ? childWidgets.first : null
+      ));
+      break;
+    case Center:
+      final List childWidgets = await visit(element, options);
+      children.add((widget as Center).toPdfWidget(
+        childWidgets.isNotEmpty ? childWidgets.first : null
+      ));
+      break;
+    case SizedBox:
+      final List childWidgets = await visit(element, options);
+      children.add((widget as SizedBox).toPdfWidget(
+        childWidgets.isNotEmpty ? childWidgets.first : null
+      ));
+      break;
+    case Padding:
+      final List childWidgets = await visit(element, options);
+      children.add((widget as Padding).toPdfWidget(
+        childWidgets.isNotEmpty ? childWidgets.first : null
+      ));
+      break;
+    case Align:
+      final List childWidgets = await visit(element, options);
+      children.add((widget as Align).toPdfWidget(
+        childWidgets.isNotEmpty ? childWidgets.first : null
+      ));
+      break;
+    case Positioned:
+      children.add((widget as Positioned).toPdfWidget((await visit(element, options)).first));
+      break;
+    case Expanded:
+      children.add((widget as Expanded).toPdfWidget((await visit(element, options)).first));
+      break;
+    case Flexible:
+      children.add((widget as Flexible).toPdfWidget((await visit(element, options)).first));
+      break;
+    case Text:
+      children.add((widget as Text).toPdfWidget());
+      break;
+    case TextField:
+      children.add((widget as TextField).toPdfWidget(options));
+      break;
+    case Divider:
+      children.add((widget as Divider).toPdfWidget());
+      break;
+    case Image:
+      children.add(await (widget as Image).toPdfWidget());
+      break;
+    case Column:
+      children.add((widget as Column).toPdfWidget(await visit(element, options)));
+      break;
+    case Row:
+      children.add((widget as Row).toPdfWidget(await visit(element, options)));
+      break;
+    case Stack:
+      children.add((widget as Stack).toPdfWidget(await visit(element, options)));
+      break;
+    case ListView:
+      children.add((widget as ListView).toPdfWidget(await visit(element, options)));
+      break;
+    case GridView:
+      children.add((widget as GridView).toPdfWidget(await visit(element, options)));
+      break;
+    default:
+      print('Uncaught: ${widget.runtimeType}');
+      final List childWidgets = await visit(element, options);
+      if (childWidgets.isNotEmpty) {
+        children.addAll(childWidgets.map((x) => x));
+      }
+      break;
+  }
+  return children;
+}
