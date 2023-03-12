75d090ee2ce7f179dc8be837a3995af6d03934fa
josephgrabinger <joseph.grabinger@yahoo.de>
2023-03-12 12:35:11 +0100

    add basic widget tests

---
+++ test/widget_basic_test.dart
@@ -0,0 +1,217 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:teaplates/teaplates.dart';
+
+
+late Document pdf;
+
+void main() {
+  setUpAll(() {
+    pdf = Document();
+  });
+
+  testWidgets('Basic Widgets Align 1', (tester) async {
+    late BuildContext exportContext;
+
+    tester.pumpWidget(Builder(
+      builder: (BuildContext context) {
+        exportContext = context;
+        return Align(
+          alignment: Alignment.bottomRight,
+          child: Container(
+            width: 200,
+            height: 400,
+            color: Colors.blue,
+          ),
+        );
+      },
+    ));
+
+    pdf.addPage(await exportToPdfPage(exportContext));
+  });
+
+  testWidgets('Basic Widgets Align 2', (tester) async {
+    late BuildContext exportContext;
+
+    tester.pumpWidget(Builder(
+      builder: (BuildContext context) {
+        exportContext = context;
+        return Align(
+          alignment: const Alignment(0.8, 0.2),
+          child: Container(
+            width: 200,
+            height: 400,
+            color: Colors.blue,
+          ),
+        );
+      },
+    ));
+
+    pdf.addPage(await exportToPdfPage(exportContext));
+  });
+
+  testWidgets('Basic Widgets AspectRatio', (tester) async {
+    late BuildContext exportContext;
+
+    tester.pumpWidget(Builder(
+      builder: (BuildContext context) {
+        exportContext = context;
+        return const AspectRatio(
+          aspectRatio: 1.618,
+          child: Placeholder(),
+        );
+      },
+    ));
+
+    pdf.addPage(await exportToPdfPage(exportContext));
+  });
+
+  testWidgets('Basic Widgets Center', (tester) async {
+    late BuildContext exportContext;
+
+    tester.pumpWidget(Builder(
+      builder: (BuildContext context) {
+        exportContext = context;
+        return Center(
+          child: Container(
+            width: 200,
+            height: 400,
+            color: Colors.blue,
+          ),
+        );
+      },
+    ));
+
+    pdf.addPage(await exportToPdfPage(exportContext));
+  });
+
+  testWidgets('Basic Widgets ConstrainedBox', (tester) async {
+    late BuildContext exportContext;
+
+    tester.pumpWidget(Builder(
+      builder: (BuildContext context) {
+        exportContext = context;
+        return ConstrainedBox(
+          constraints: const BoxConstraints.tightFor(height: 300),
+          child: const Placeholder(),
+        );
+      },
+    ));
+
+    pdf.addPage(await exportToPdfPage(exportContext));
+  });
+
+  testWidgets('Basic Widgets FittedBox', (tester) async {
+    late BuildContext exportContext;
+
+    tester.pumpWidget(Builder(
+      builder: (BuildContext context) {
+        exportContext = context;
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            for (final fit in BoxFit.values) SizedBox(
+              height: 100,
+              width: 100,
+              child: FittedBox(
+                fit: fit,
+                child: const SizedBox(
+                  width: 100,
+                  height: 50,
+                  child: Placeholder(),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    ));
+
+    pdf.addPage(await exportToPdfPage(exportContext));
+  });
+
+  testWidgets('Basic Widgets LimitedBox', (tester) async {
+    late BuildContext exportContext;
+
+    tester.pumpWidget(Builder(
+      builder: (BuildContext context) {
+        exportContext = context;
+        return ListView(
+          scrollDirection: Axis.vertical,
+          children: const [
+            LimitedBox(
+              maxHeight: 40,
+              child: Placeholder(),
+            ),
+          ],
+        );
+      },
+    ));
+
+    pdf.addPage(await exportToPdfPage(exportContext));
+  });
+
+  testWidgets('Basic Widgets Padding', (tester) async {
+    late BuildContext exportContext;
+
+    tester.pumpWidget(Builder(
+      builder: (BuildContext context) {
+        exportContext = context;
+        return Padding(
+          padding: const EdgeInsets.all(20),
+          child: Container(
+            width: 200,
+            height: 400,
+            color: Colors.blue,
+          ),
+        );
+      },
+    ));
+
+    pdf.addPage(await exportToPdfPage(exportContext));
+  });
+
+  testWidgets('Basic Widgets SizedBox', (tester) async {
+    late BuildContext exportContext;
+
+    tester.pumpWidget(Builder(
+      builder: (BuildContext context) {
+        exportContext = context;
+        return const SizedBox(
+          width: 200,
+          height: 100,
+          child: Placeholder(),
+        );
+      },
+    ));
+
+    pdf.addPage(await exportToPdfPage(exportContext));
+  });
+
+  testWidgets('Basic Widgets Transform', (tester) async {
+    late BuildContext exportContext;
+
+    tester.pumpWidget(Builder(
+      builder: (BuildContext context) {
+        exportContext = context;
+        return Transform.scale(
+          scale: 0.5,
+          child: Transform.rotate(
+            angle: 0.1,
+            child: const Placeholder(),
+          ),
+        );
+      },
+    ));
+
+    pdf.addPage(await exportToPdfPage(exportContext));
+  });
+
+  tearDownAll(() async {
+    final file = File('./test/output/widgets-basic.pdf');
+    file.writeAsBytesSync(await pdf.save());
+  });
+}
