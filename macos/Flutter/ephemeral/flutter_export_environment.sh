3eabe9bf0483dc4a73f423d4a4e9769fa79f3538
josephgrabinger <joseph.grabinger@yahoo.de>
2023-03-28 16:30:19 +0200

    upgrade pdf dependency

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/josephgrabinger/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/josephgrabinger/Documents/Dunef/Projekte/teaplates"
+export "FLUTTER_APPLICATION_PATH=/Users/josephgrabinger/Documents/Dunef/Projekte/flutter_to_pdf"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
