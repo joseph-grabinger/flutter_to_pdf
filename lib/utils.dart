a46f0883a764d19f2b4e9ec14a3972bfbd529287
Vera Spöttl-Zeisberg <vsz@zeisberg.net>
2024-05-14 11:10:32 +0200

    fixed bug in utils.dart (breaking change in ViewConfiguration https://github.com/flutter/flutter/issues/141562)

--- lib/utils.dart
+++ lib/utils.dart
@@ -66,7 +66,8 @@ Element? layoutWidget(Widget widget, Size size, double? pixelRatio) {
 
   RenderView renderView = RenderView(
     configuration: ViewConfiguration(
-      size: size,
+      logicalConstraints: BoxConstraints.tight(size),
+      physicalConstraints: BoxConstraints.tight(size * (pixelRatio ?? 1.0)),
       devicePixelRatio: pixelRatio ?? 1.0,
     ),
     view: WidgetsBinding.instance.platformDispatcher.views.first,
