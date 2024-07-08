2f3b0bcd8ecb40030f09d3952d4465344ace7fbd
sousa-p <p.soousa@gmail.com>
2024-07-08 12:03:34 -0300

    🐛 fix: container edge cast errors

--- lib/args/alignment.dart
+++ lib/args/alignment.dart
@@ -2,6 +2,7 @@ import 'package:flutter/widgets.dart' show Alignment, AlignmentDirectional;
 
 import 'package:pdf/widgets.dart' as pw show Alignment;
 
+
 extension AlignmentConverter on Alignment {
   pw.Alignment toPdfAlignment() => pw.Alignment(x, y);
 }
--- lib/widgets/container.dart
+++ lib/widgets/container.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/widgets.dart'
-    show Alignment, BoxDecoration, Container, EdgeInsets, AlignmentDirectional;
+    show
+        Alignment,
+        BoxDecoration,
+        Container,
+        EdgeInsets,
+        AlignmentDirectional,
+        AlignmentGeometry,
+        EdgeInsetsGeometry,
+        EdgeInsetsDirectional;
+import 'package:flutter_to_pdf/args/alignment.dart';
+import 'package:flutter_to_pdf/args/edge_insets_directional.dart';
 
 import 'package:pdf/widgets.dart' as pw show Container, Widget;
 
 import '../args/box_decoration.dart';
-import '../args/alignment.dart';
 import '../args/color.dart';
 import '../args/box_constraints.dart';
 import '../args/edge_insets.dart';
@@ -13,19 +22,27 @@ import '../args/edge_insets.dart';
 extension ContainerConverter on Container {
   /// Converts the [Container] to a [pw.Container].
   Future<pw.Container> toPdfWidget(pw.Widget? child) async => pw.Container(
-        alignment: (alignment is Alignment?)
-            ? (alignment as Alignment?)?.toPdfAlignment()
-            : (alignment is AlignmentDirectional)
-                ? (alignment as AlignmentDirectional?)?.toPdfAlignment()
-                : null,
+        alignment: _convertAlignment(alignment),
         decoration: await (decoration as BoxDecoration?)?.toPdfBoxDecoration(),
         color: color?.toPdfColor(),
         constraints: constraints?.toPdfBoxConstraints(),
         foregroundDecoration: await (foregroundDecoration as BoxDecoration?)
             ?.toPdfBoxDecoration(),
-        margin: (margin as EdgeInsets?)?.toPdfEdgeInsets(),
-        padding: (padding as EdgeInsets?)?.toPdfEdgeInsets(),
+        margin: _convertEdge(margin),
+        padding:_convertEdge(padding),
         transform: transform,
         child: child,
       );
+
+  _convertAlignment(AlignmentGeometry? alignment) {
+    if (alignment is Alignment) return alignment.toPdfAlignment();
+    if (alignment is AlignmentDirectional) return alignment.toPdfAlignment();
+    return null;
+  }
+
+  _convertEdge(EdgeInsetsGeometry? edge) {
+    if (edge is EdgeInsetsDirectional) return edge.toPdfEdgeInsetsDirectional();
+    if (edge is EdgeInsets) return edge.toPdfEdgeInsets();
+    return null;
+  }
 }
