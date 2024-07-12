7207a793bf6677001eac4560079e0e36bafaa914
Pedro S <97417230+sousa-p@users.noreply.github.com>
2024-07-12 13:44:16 -0300

    ♻️ refactor: more reusable impl of .toPdfAligment
    Co-authored-by: Joseph Grabinger <joseph.grabinger@yahoo.de>

--- lib/args/alignment.dart
+++ lib/args/alignment.dart
@@ -2,6 +2,19 @@ import 'package:flutter/widgets.dart' show Alignment, AlignmentDirectional;
 
 import 'package:pdf/widgets.dart' as pw show Alignment;
 
+extension AlignmentGeometryConverter on AlignmentGeometry {
+  pw.Alignment? toPdfAlignment() {
+    switch (runtimeType) {
+      case const (Alignment):
+        return (this as Alignment).toPdfAlignment();
+      case const (AlignmentDirectional):
+        return (this as AlignmentDirectional).toPdfAlignment();
+      default:
+        debugPrint('Unsupported AlignmentGeometry: $this; defaulting to null');
+        return null;
+    }
+  }
+}
 
 extension AlignmentConverter on Alignment {
   pw.Alignment toPdfAlignment() => pw.Alignment(x, y);
