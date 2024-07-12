a34a56a33ebf584ef8f264f122dd6d5eb2ef180a
sousa-p <p.soousa@gmail.com>
2024-07-12 14:16:18 -0300

    🚀 feat: better impl to edge_insets.dart

--- lib/widgets/container.dart
+++ lib/widgets/container.dart
@@ -1,18 +1,6 @@
-import 'package:flutter/widgets.dart'
-    show
-        Alignment,
-        BoxDecoration,
-        Container,
-        EdgeInsets,
-        AlignmentDirectional,
-        AlignmentGeometry,
-        EdgeInsetsGeometry,
-        EdgeInsetsDirectional,
-        debugPrint;
+import 'package:flutter/widgets.dart' show BoxDecoration, Container;
 
 import 'package:flutter_to_pdf/args/alignment.dart';
-import 'package:flutter_to_pdf/args/edge_insets_directional.dart';
-
 import 'package:pdf/widgets.dart' as pw show Container, Widget;
 
 import '../args/box_decoration.dart';
@@ -24,29 +12,15 @@ import '../args/edge_insets.dart';
 extension ContainerConverter on Container {
   /// Converts the [Container] to a [pw.Container].
   Future<pw.Container> toPdfWidget(pw.Widget? child) async => pw.Container(
-        alignment: _convertAlignment(alignment),
+        alignment: alignment?.toPdfAlignment(),
         decoration: await (decoration as BoxDecoration?)?.toPdfBoxDecoration(),
         color: color?.toPdfColor(),
         constraints: constraints?.toPdfBoxConstraints(),
         foregroundDecoration: await (foregroundDecoration as BoxDecoration?)
             ?.toPdfBoxDecoration(),
-        margin: _convertEdge(margin),
-        padding: _convertEdge(padding),
+        margin: margin?.toPdfEdgeInsets(),
+        padding: padding?.toPdfEdgeInsets(),
         transform: transform,
         child: child,
       );
-
-  _convertAlignment(AlignmentGeometry? alignment) {
-    if (alignment is Alignment) return alignment.toPdfAlignment();
-    if (alignment is AlignmentDirectional) return alignment.toPdfAlignment();
-    debugPrint('Unsupported AlignmentGeometry: $this; defaulting to null');
-    return null;
-  }
-
-  _convertEdge(EdgeInsetsGeometry? edge) {
-    if (edge is EdgeInsetsDirectional) return edge.toPdfEdgeInsetsDirectional();
-    if (edge is EdgeInsets) return edge.toPdfEdgeInsets();
-    debugPrint('Unsupported EdgeInsetsGeometry: $this; defaulting to null');
-    return null;
-  }
 }
--- lib/widgets/padding.dart
+++ lib/widgets/padding.dart
@@ -1,14 +1,6 @@
-import 'package:flutter/widgets.dart'
-    show
-        EdgeInsets,
-        EdgeInsetsDirectional,
-        Padding,
-        EdgeInsetsGeometry,
-        debugPrint;
+import 'package:flutter/widgets.dart' show Padding;
 
-import 'package:flutter_to_pdf/args/edge_insets_directional.dart';
-
-import 'package:pdf/widgets.dart' as pw show Padding, Widget;
+import 'package:pdf/widgets.dart' as pw show Padding, Widget, EdgeInsets;
 
 import '/args/edge_insets.dart';
 
@@ -16,16 +8,7 @@ import '/args/edge_insets.dart';
 extension PaddingConverter on Padding {
   /// Converts the [Padding] to a [pw.Padding].
   pw.Padding toPdfWidget(pw.Widget? child) => pw.Padding(
-        padding: _convertEdge(padding),
+        padding: padding.toPdfEdgeInsets() ?? const pw.EdgeInsets.all(0),
         child: child,
       );
-
-  _convertEdge(EdgeInsetsGeometry? edge) {
-    if (edge is EdgeInsetsDirectional) return edge.toPdfEdgeInsetsDirectional();
-    if (edge is EdgeInsets) return edge.toPdfEdgeInsets();
-    debugPrint(
-      'Unsupported EdgeInsetsGeometry: $this; defaulting to EdgeInsets.all(8.0)',
-    );
-    return const EdgeInsets.all(8.0);
-  }
 }
